61076334c414d979942434ad4fa2ba76fc404f17
Gregor Pogačnik <1640719+fiksn@users.noreply.github.com>
2019-05-04 16:07:16 +0200

    ...

--- nixops/trivial.nix
+++ nixops/trivial.nix
@@ -7,6 +7,10 @@
       programs.mosh.enable = true;
       virtualisation.docker.enable = true;
       environment.systemPackages = with pkgs; [ docker docker_compose skopeo docker-ls ];
+      boot.kernel.sysctl = {
+        "net.ipv4.ip_forward" = 1;
+        "vm.overcommit_memory" = 1;
+      };
 
       services.ssh.enable = true;
       networking.firewall.allowedTCPPorts = [ 22 ]; 
