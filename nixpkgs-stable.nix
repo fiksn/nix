6090faae116c2fd75c43792959b6c78cce750f93
Gregor Pogačnik <1640719+fiksn@users.noreply.github.com>
2020-11-18 14:25:46 +0100

    Upgrade

--- nixpkgs-stable.nix
+++ nixpkgs-stable.nix
@@ -12,7 +12,7 @@
 
 import (builtins.fetchTarball {
   name = "nixpkgs-stable";
-  url = https://github.com/NixOS/nixpkgs/archive/cae7840b7639c56ad619cb8fb8f921298ae385b7.tar.gz;
-  sha256 = "1mr5bp3vnag9nihcz1l0hdvm2n8n7lm84qh3cynbp00rcpwy0mhc";
+  url = https://github.com/NixOS/nixpkgs/archive/cd63096d6d887d689543a0b97743d28995bc9bc3.tar.gz;
+  sha256 = "sha256:1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
 }) {}
 
